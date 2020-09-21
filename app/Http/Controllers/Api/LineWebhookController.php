4ae77d6227a8da3a30df30db1c8b16731aaa926c
榑林晃平 <kureykou321@gmail.com>
2020-09-21 18:07:08 +0900

    ログを出力するように設定

--- app/Http/Controllers/Api/LineWebhookController.php
+++ app/Http/Controllers/Api/LineWebhookController.php
@@ -51,8 +51,8 @@ class LineWebhookController extends Controller
         $checkMessage = is_null($message) ? "nullやないかい": $message;
         $replyMessage = new TextMessageBuilder($checkMessage);
 
-        $a = $lineBot->replyMessage($replyToken, $replyMessage);
-        Log::info(var_export($replyMessage, true));
+        $lineBot->replyMessage($replyToken, $replyMessage);
+        Log::info(var_export($lineBot, true));
 
       }
     } catch (Exception $e) {
